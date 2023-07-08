72446f916acea277c7819ce37b41377054d1a6fe
Fine Lee <juck0722@gmail.com>
2023-07-08 14:05:17 +0900

    commit

--- src/Chap_4/Ex4_14.java
+++ src/Chap_4/Ex4_14.java
@@ -1,4 +1,22 @@
 package Chap_4;
+import java.util.*;
 
 public class Ex4_14 {
+    public static void main(String[] args) {
+        int num = 0, sum = 0;
+        System.out.print("숫자를 입력하세요.(예:12345)>");
+
+        Scanner scanner = new Scanner(System.in);
+        String tmp = scanner.nextLine(); // 화면으로 입력받는 내용을 변수 tmp에 저장
+        num = Integer.parseInt(tmp); // 입력받은 문자열(tmp)을 숫자로 변환
+
+
+        while(num != 0) {
+            sum = sum + num%10;
+            System.out.printf("sum=%3d num=%d%n", sum, num);
+
+            num = num / 10;
+        }
+        System.out.println("각 자리수의 합: " + sum); // 12345 입력 시 : 1+2+3+4+5
+    }
 }
--- src/Chap_4/Ex4_16.java
+++ src/Chap_4/Ex4_16.java
@@ -1,4 +1,17 @@
 package Chap_4;
 
 public class Ex4_16 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 0;
+
+        while(true) {
+            if(sum > 100)
+                break;    // break문이 실행되면 밑의 2줄은 실행되지 않고 while문을 벗어난다.
+            ++i;
+            sum = sum+i;
+        }
+        System.out.println("i=" + i);
+        System.out.println("sum=" + sum);
+    }
 }
