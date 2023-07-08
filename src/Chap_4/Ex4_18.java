5286c7c9401f796abf57041795314c77f8e52c82
Fine Lee <juck0722@gmail.com>
2023-07-08 15:37:36 +0900

    commit

--- src/Chap_4/Ex4_18.java
+++ src/Chap_4/Ex4_18.java
@@ -3,26 +3,26 @@ import java.util.*;
 
 public class Ex4_18 {
     public static void main(String[] args) {
-        int menu = 0;
+        int menu = 0; // 변수 선언 및 초기화
         int num = 0;
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // 사용자 입력 세팅
 
-        while(true) {
+        while(true) { // while(true) : 무한반복문 -> break 꼭 필요
             System.out.println("(1) square");
-            System.out.println("(1) square root");
-            System.out.println("(1) log");
+            System.out.println("(2) square root");
+            System.out.println("(3) log");
             System.out.print("원하는 메뉴(1~3)를 선택하세요.(종료:0)>");
 
-            String tmp = scanner.nextLine();
-            menu = Integer.parseInt(tmp);
+            String tmp = scanner.nextLine(); // 입력 받은 값 저장
+            menu = Integer.parseInt(tmp); // 문자열로 받은 tmp값 -> 정수형 menu로 변환
 
-            if (menu==0) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
-            } else if (!(1 <= menu && menu <= 3)) {
-                System.out.println("메뉴를 잘못 선택하셨습니다.(종료는 0)");
-                continue;
+            if (menu==0) { // menu에서 0을 입력했을 경우
+                System.out.println("프로그램을 종료합니다."); // 프로그램 종료 멘트 후
+                break; // while문 탈출
+            } else if (!(1 <= menu && menu <= 3)) { // menu를 1~3번이 아닌 것을 입력했을 경우
+                System.out.println("메뉴를 잘못 선택하셨습니다.(종료는 0)"); // 잘못 선택 멘트 후
+                continue; // 다음 반복으로 넘어가서 다시 물어보기
             }
 
             System.out.println("선택하신 메뉴는 " + menu + "번입니다.");
--- src/Chap_4/Ex4_20.java
+++ src/Chap_4/Ex4_20.java
@@ -1,4 +1,54 @@
 package Chap_4;
+import java.util.*;
 
 public class Ex4_20 {
+    public static void main(String[] args) {
+        int menu = 0, num = 0; // 변수 선언 및 초기화
+
+        Scanner scanner = new Scanner(System.in); // 사용자 입력 세팅
+
+        outer: // while문에 outer라는 이름을 붙임
+        while(true) { // while(true) : 무한반복문 -> break 꼭 필요
+            System.out.println("(1) square");
+            System.out.println("(2) square root");
+            System.out.println("(3) log");
+            System.out.print("원하는 메뉴(1~3)를 선택하세요.(종료:0)>");
+
+            String tmp = scanner.nextLine(); // 사용자한테 입력받은 값을 변수 tmp에 저장
+            menu = Integer.parseInt(tmp); // 문자열로 받은 tmp값 -> 정수형으로 변환해서 변수 menu에 저장
+
+
+            if (menu == 0) { // menu에서 0을 입력했을 경우
+                System.out.println("프로그램을 종료합니다."); // 프로그램 종료 멘트 후
+                break; // while문 탈출
+            } else if (!(1 <= menu && menu <= 3)) { // menu를 1~3번이 아닌 것을 입력했을 경우
+                System.out.println("메뉴를 잘못 선택하셨습니다.(종료는 0)"); // 잘못 선택 멘트 후
+                continue; // 다음 반복으로 넘어가서 다시 물어보기
+            }
+
+
+            for(;;) { // 무한반복문 -> break 꼭 필요
+                System.out.print("계산할 값을 입력하세요.(계산 종료:0, 전체 종료:99)>");
+                tmp = scanner.nextLine();
+                num = Integer.parseInt(tmp);
+
+                if(num==0)
+                    break;
+                if(num==99)
+                    break outer;
+
+                switch(menu) {
+                    case 1:
+                        System.out.println("result=" + num*num);
+                        break;
+                    case 2:
+                        System.out.println("result=" + Math.sqrt(num));
+                        break;
+                    case 3:
+                        System.out.println("result=" + Math.log(num));
+                        break;
+                }
+            }
+        }
+    }
 }
